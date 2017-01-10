a85c74d0ffee0fa4bc036eca23908ec9e5a608ab
justnoxx <justnoxx@gmail.com>
2017-01-10 17:48:05 +0200

    added additional fields to check deploy status

--- src/main/resources/project/server/checkDeployStatus.pl
+++ src/main/resources/project/server/checkDeployStatus.pl
@@ -21,7 +21,24 @@ sub main {
 
     my $params = $jboss->get_params_as_hashref(qw/
         appname
+        servers
+        serversgroup
+        criteria
+        wait_time
     /);
+
+    my @servers = ();
+    my @server_groups = ();
+
+    if ($params->{servers}) {
+        print "Servers.\n";
+        @servers = map {$jboss->trim($_); $_;} split(',', $params->{servers});
+    }
+
+    if ($params->{serversgroup}) {
+        print "Server group.\n";
+        @server_groups = map {$jboss->trim($_); $_;} split(',', $params->{serversgroup});
+    }
 
     my $appname = $params->{appname};
     my $launch_type = $jboss->get_launch_type();
@@ -29,31 +46,52 @@ sub main {
     my $servers = {};
     if ($launch_type eq 'domain') {
         # $server_groups = $jboss->get_server_groups();
-        $servers = $jboss->get_servers();
+        $servers = $jboss->get_servers(
+            hosts => undef,
+            servers => \@servers,
+            groups => \@server_groups
+        );
     }
+
     # logic for domain check
-    my @errors = ();
     if ($launch_type eq 'domain') {
-        for my $host (keys %$servers) {
-            for my $server (@{$servers->{$host}}) {
-                my $command = sprintf '/host=%s/server=%s/deployment=%s:read-attribute(name=status)', $host, $server, $appname;
-                my %result = $jboss->run_command($command);
-                if ($result{code}) {
-                    $jboss->out("Application $appname (server: $server, host: $host is NOT OK");
-                    push @errors, $server;
-                    next;
+        my @errors = ();
+        my $result = $jboss->run_commands_until_done({
+            sleep_time => 5,
+            time_limit => $params->{wait_time},
+        }, sub {
+            @errors = ();
+            for my $host (keys %$servers) {
+                if (!@{$servers->{$host}}) {
+                    $jboss->bail_out("No servers were found by your input for host: $host.");
                 }
+                for my $server (@{$servers->{$host}}) {
+                    my $command = sprintf '/host=%s/server=%s/deployment=%s:read-attribute(name=status)', $host, $server, $appname;
+                    my %result = $jboss->run_command($command);
+                    if ($result{code}) {
+                        $jboss->out("Application $appname (server: '$server', host: '$host') is NOT OK. Criteria was not met.");
+                        push @errors, $server;
+                        next;
+                    }
 
-                my $json = $jboss->decode_answer($result{stdout});
-                if ($json->{result} ne 'OK') {
-                    $jboss->out("Application $appname (server: '$server', host: '$host') is NOT OK");
-                    push @errors, $server;
+                    my $json = $jboss->decode_answer($result{stdout});
+                    if (!is_criteria_met_domain($json->{result}, $params->{criteria})) {
+                        # if ($json->{result} ne 'OK') {
+                        $jboss->out("Application $appname (server: '$server', host: '$host') is NOT OK. Criteria was not met.");
+                        push @errors, $server;
+                    }
+                    $jboss->out("Application $appname (server: '$server', host: '$host') has status: $json->{result}. Desired: $params->{criteria}");
                 }
-                $jboss->out("Application $appname (server: $server, host: $host is $json->{result}");
             }
-        }
-
-        if (@errors) {
+            if (@errors) {
+                my $msg = 'Wrong application status on the following servers: ' . join (', ', @errors);
+                $jboss->out($msg);
+                return 0;
+                # $jboss->bail_out($msg);
+            }
+            return 1;
+        });
+        if (!$result) {
             my $msg = 'Wrong application status on the following servers: ' . join (', ', @errors);
             $jboss->bail_out($msg);
         }
@@ -62,6 +100,47 @@ sub main {
     # logic for standalone mode
 
     my $command = "/deployment=$params->{appname}:read-attribute(name=status)";
-    my %result = $jboss->run_command($command);
+    my %result;
+    $jboss->run_commands_until_done({
+        time_limit => $params->{wait_time},
+        sleep_time => 5,
+    }, sub {
+        %result = $jboss->run_command($command);
+        my $json = $jboss->decode_answer($result{stdout});
+        if (!is_criteria_met_standalone($json, $params->{criteria})) {
+            $jboss->out("Criteria was not met. Application status: $result{stdout}");
+            return 0;
+        }
+        return 1;
+    });
     $jboss->process_response(%result);
 };
+
+sub is_criteria_met_standalone {
+    my ($json, $criteria) = @_;
+
+    if ($criteria eq 'OK') {
+        if ($json && $json->{outcome} && $json->{result} eq 'OK') {
+            return 1;
+        }
+        return 0;
+    }
+    else {
+        if ($json && $json->{outcome} && $json->{result} eq 'OK') {
+            return 0;
+        }
+        return 1;
+    }
+
+}
+sub is_criteria_met_domain {
+    my ($got, $expected) = @_;
+
+    if ($expected eq 'OK') {
+        return 1 if $got eq $expected;
+    }
+    else {
+        return 1 if $got ne 'OK';
+    }
+    return 0;
+}
