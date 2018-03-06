f0de1128f26b503222d38348acb4c32ae7fd80b7
Victorii <victoria.maziar@gmail.com>
2018-03-06 15:35:18 +0200

    tmp --need revert

--- specs/RemoveJMSTopicDomain.groovy
+++ specs/RemoveJMSTopicDomain.groovy
@@ -1,7 +1,7 @@
 import Services.CliCommandsGeneratorHelper
 import Utils.EnvPropertiesHelper
 import spock.lang.*
-
+@Ignore
 @IgnoreIf({ env.JBOSS_MODE == 'standalone' })
 class RemoveJMSTopicDomain extends PluginTestHelper {
 
