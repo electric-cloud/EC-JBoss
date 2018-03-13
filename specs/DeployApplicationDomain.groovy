82ad2c0481501bd18e177b76bde061fdfd9d2438
Victorii <victoria.maziar@gmail.com>
2018-03-13 11:55:04 +0200

    ECPAPPSERVERJBOSS-615 Fix system test DeployApplication, app already deployed, url (for EAP 7 and later) C278258'

--- specs/DeployApplicationDomain.groovy
+++ specs/DeployApplicationDomain.groovy
@@ -306,7 +306,7 @@ class DeployApplicationDomain extends PluginTestHelper {
 
                  then:
                  assert runProcedureJob.getStatus() == "success"
-                 assert runProcedureJob.getUpperStepSummary() =~ "Application '$existingAppName' has been successfully deployed from '$linkToSampleWarFile'.\nEnabled on: $serverGroup2 server groups.\nDisabled on: $serverGroup1 server groups."
+                 assert runProcedureJob.getUpperStepSummary() =~ "Application '$existingAppName' has been successfully deployed from '$linkToSampleWarFile2'.\nEnabled on: $serverGroup2 server groups.\nDisabled on: $serverGroup1 server groups."
 
                  String[] expectedServerGroupsWithAppEnabled = [serverGroup2]
                  String[] expectedServerGroupsWithAppDisabled = [serverGroup1]
