a89e70a898ce1736404062b08afc3648544a16c8
Victorii <victoria.maziar@gmail.com>
2018-03-13 13:04:54 +0200

    ECPAPPSERVERJBOSS-599

--- specs/DeployAppDomain.groovy
+++ specs/DeployAppDomain.groovy
@@ -46,7 +46,7 @@ class DeployAppDomain extends PluginTestHelper {
         logger.info("win "+EnvPropertiesHelper.isWindows())
         String warphysicalpath = "/tmp/"
         if(EnvPropertiesHelper.isWindows()){
-            warphysicalpath = "С:\\tmp\\"
+            warphysicalpath = "C:\\tmp\\"
         }
         return warphysicalpath
     }
