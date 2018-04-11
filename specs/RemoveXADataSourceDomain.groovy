4ca27cfd9e529a6b928f7f7cc98c4a1fd481e1ff
Ed Serbin <eserbin@electric-cloud.com>
2018-04-11 17:22:33 +0300

    changes

--- specs/RemoveXADataSourceDomain.groovy
+++ specs/RemoveXADataSourceDomain.groovy
@@ -13,6 +13,25 @@ class RemoveXADataSourceDomain extends PluginTestHelper {
     String defaultConfigName = 'specConfig'
     @Shared
     String defaultProfile = 'full'
+    @Shared
+    def link = [
+            /**
+             * Required
+             */
+            empty: '',
+            mysql: "https://github.com/electric-cloud/hello-world-war/raw/system_tests/dist/XADatasources/mysql/mysql-connector-java-5.1.36.jar",
+            postgresql: "https://github.com/electric-cloud/hello-world-war/raw/system_tests/dist/XADatasources/postgresql/postgresql-42.2.2.jar",
+    ]
+
+    @Shared
+    def xml = [
+            /**
+             * Required
+             */
+            empty: '',
+            mysql: "https://github.com/electric-cloud/hello-world-war/raw/system_tests/dist/XADatasources/mysql/module.xml",
+            postgresql: "https://github.com/electric-cloud/hello-world-war/raw/system_tests/dist/XADatasources/postgresql/module.xml",
+    ]
 
 
     def doSetupSpec() {
@@ -46,6 +65,30 @@ class RemoveXADataSourceDomain extends PluginTestHelper {
         return runProcedureDsl(projectName, procName, parameters)
     }
 
+    static String getPathToMain(String path, String domain) {
+        String pathForJar = ''
+        if(EnvPropertiesHelper.getVersion() == "6.0"){
+            pathForJar = "/opt/jboss/modules/$domain/$path/main"
+            EnvPropertiesHelper.getOS() == "WINDOWS" ? pathForJar = "C:\\\\opt\\\\jboss\\\\modules\\\\$domain\\\\$path\\\\main" : pathForJar
+        } else {
+            pathForJar = "/opt/jboss/modules/system/layers/base/$domain/$path/main"
+            EnvPropertiesHelper.getOS() == "WINDOWS" ? pathForJar = "C:\\\\opt\\\\jboss\\\\modules\\\\system\\\\layers\\\\base\\\\$domain\\\\$path\\\\main" : pathForJar
+        }
+        return  pathForJar
+    }
+
+    static String getPath(String path, String domain) {
+        String pathForJar = ''
+        if(EnvPropertiesHelper.getVersion() == "6.0"){
+            pathForJar = "/opt/jboss/modules/$domain/$path"
+            EnvPropertiesHelper.getOS() == "WINDOWS" ? pathForJar = "C:\\\\opt\\\\jboss\\\\modules\\\\$domain\\\\$path" : pathForJar
+        } else {
+            pathForJar = "/opt/jboss/modules/system/layers/base/$domain/$path"
+            EnvPropertiesHelper.getOS() == "WINDOWS" ? pathForJar = "C:\\\\opt\\\\jboss\\\\modules\\\\system\\\\layers\\\\base\\\\$domain\\\\$path" : pathForJar
+        }
+        return  pathForJar
+    }
+    
     @Unroll
     def "RemoveXADataSource, MySQL C289593"() {
         String testCaseId = "C278471"
@@ -55,9 +98,16 @@ class RemoveXADataSourceDomain extends PluginTestHelper {
                 dataSourceName   : 'MysqlXADS',
         ]
         setup:
+        String path = getPathToMain("mysql", "com")
+        createDir(getPath("mysql", "com"))
+        createDir(path)
+        downloadArtifact(link.mysql, path+"/mysql-connector-java-5.1.36.jar")
+        downloadArtifact(xml.mysql, path+"/module.xml")
         
     }
     
-
+    void addModuleXADatasource(String profile, String driver, String DSclass){
+        runCliCommand(CliCommandsGeneratorHelper.addModuleXADatasource(profile, driver, DSclass))
+    }
 
 }
