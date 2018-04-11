85582068e1ed2384087987c4135a3973f9e262ad
Ed Serbin <eserbin@electric-cloud.com>
2018-04-11 16:18:13 +0300

    start remove XA

---
+++ specs/RemoveXADataSourceDomain.groovy
@@ -0,0 +1,60 @@
+import Services.CliCommandsGeneratorHelper
+import Utils.EnvPropertiesHelper
+import spock.lang.*
+
+@IgnoreIf({ env.JBOSS_MODE == 'standalone' })
+class RemoveXADataSourceDomain extends PluginTestHelper {
+
+	@Shared
+    String procName = 'RemoveXADataSource'
+    @Shared
+    String projectName = "EC-JBoss Specs $procName Project"
+    @Shared
+    String defaultConfigName = 'specConfig'
+    @Shared
+    String defaultProfile = 'full'
+
+
+    def doSetupSpec() {
+        logger.info("Hello World! doSetupSpec")
+        redirectLogs()
+        createDefaultConfiguration(defaultConfigName)
+        def resName = createJBossResource()
+
+        dslFile 'dsl/RunProcedure.dsl', [
+                projName: projectName,
+                resName : resName,
+                procName: procName,
+                params  : [
+                        profile      : '',
+                        serverconfig : '',
+                        dataSourceName:    : '',
+                ]
+        ]
+
+        createHelperProject(resName, defaultConfigName)
+
+    }
+
+    def doCleanupSpec() {
+        logger.info("Hello World! doCleanupSpec")
+        deleteProject(projectName)
+        deleteConfiguration("EC-JBoss", defaultConfigName)
+    }
+
+    RunProcedureJob runProcedureUnderTest(def parameters) {
+        return runProcedureDsl(projectName, procName, parameters)
+    }
+
+    @Unroll
+    def "RemoveXADataSource, MySQL C289593"() {
+        String testCaseId = "C278471"
+        def runParams = [
+                profile          : defaultProfile,
+                serverconfig     : defaultConfigName,
+                dataSourceName        : 'MysqlXADS',
+        ]  
+    
+
+
+}
