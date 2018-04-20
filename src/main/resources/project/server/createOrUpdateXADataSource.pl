3daac41af3e26a8a6e2939e2072a1c40611eb563
Olexii Vasilkovsky <ovasilko@electric-cloud.com>
2018-04-20 13:42:59 +0300

    ECPAPPSERVERJBOSS-691 fix for enabling data source in 6.0

--- src/main/resources/project/server/createOrUpdateXADataSource.pl
+++ src/main/resources/project/server/createOrUpdateXADataSource.pl
@@ -272,6 +272,24 @@ sub main {
             $jboss->set_property(summary => $summary);
         }
 
+        ########
+        # workaround for 6.0 (enable=true within xa-data-source add command does not take affect, data source created and disabled)
+        ########
+        if ($param_enabled) {
+            my $version = $jboss->get_jboss_server_version();
+            my $product_version = $version->{product_version};
+            if ($product_version =~ m/^6\.0/ ) {
+                $jboss->log_info("Enabling data source within 6.0 separately due to known issue (enable=true within xa-data-source add command does not take affect, data source created and disabled)");
+
+                my $command_enable_data_source = "$profile_prefix/subsystem=datasources/xa-data-source=$param_data_source_name/:enable";
+                my %result_enable_data_source = $jboss->run_command($command_enable_data_source);
+                $jboss->process_response(%result_enable_data_source);
+                if ($result{code}) {
+                    exit 1;
+                }
+            }
+        }
+
         return;
     }
 }
