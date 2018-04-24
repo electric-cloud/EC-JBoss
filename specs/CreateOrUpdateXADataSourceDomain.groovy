af12cbaa855e8f4bbbf81722f4d1ab1a6889361e
Ed Serbin <eserbin@electric-cloud.com>
2018-04-24 16:41:17 +0300

    removed typo

--- specs/CreateOrUpdateXADataSourceDomain.groovy
+++ specs/CreateOrUpdateXADataSourceDomain.groovy
@@ -2,7 +2,7 @@ import Services.CliCommandsGeneratorHelper
 import Utils.EnvPropertiesHelper
 import spock.lang.*
 
-@IgnoreIf({env.EnvPropertiesHelper.getVersion() == "6.3"})
+@IgnoreIf({EnvPropertiesHelper.getVersion() == "6.3"})
 @Requires({ env.JBOSS_TOPOLOGY == 'master' })
 class CreateOrUpdateXADataSourceDomain extends PluginTestHelper {
 
