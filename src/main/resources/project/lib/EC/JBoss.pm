58fada3470aee4709d47d4fd44a1b78dd058d0d1
Olexii Vasilkovsky <ovasilko@electric-cloud.com>
2018-05-24 15:03:34 +0300

    ECPAPPSERVERJBOSS-744 minor fix

--- src/main/resources/project/lib/EC/JBoss.pm
+++ src/main/resources/project/lib/EC/JBoss.pm
@@ -64,7 +64,7 @@ $| = 1;
             );
     }
 
-    ElectricCommander::PropMod::loadPerlCodeFromProperty($tec,"/myProject/jboss_driver/EC::Bootstrap.pm");
+    ElectricCommander::PropMod::loadPerlCodeFromProperty($ec,"/myProject/jboss_driver/EC::Bootstrap.pm");
 };
 
 =item B<new>
