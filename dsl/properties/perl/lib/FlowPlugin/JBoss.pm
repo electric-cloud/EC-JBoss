6b63e6241d37f3d0d8016f920653bd4f6db3b400
Andrii Pravorskyi <apravorskyi@cloudbees.com>
2022-04-28 18:02:25 +0300

    PDK: port UndeployApp procedure

--- dsl/properties/perl/lib/FlowPlugin/JBoss.pm
+++ dsl/properties/perl/lib/FlowPlugin/JBoss.pm
@@ -3792,8 +3792,50 @@ sub undeployApp {
     my $configValues = $context->getConfigValues();
     logInfo("Config values are: ", $configValues);
 
-    $sr->setJobStepOutcome('warning');
-    $sr->setJobSummary("This is a job summary.");
+    my $PROJECT_NAME = '$[/myProject/projectName]';
+    my $PLUGIN_NAME = '@PLUGIN_NAME@';
+    my $PLUGIN_KEY = '@PLUGIN_KEY@';
+
+    my $jboss = EC::JBoss->new(
+        project_name => $PROJECT_NAME,
+        plugin_name  => $PLUGIN_NAME,
+        plugin_key   => $PLUGIN_KEY,
+        flowpdf      => $self,
+    );
+
+    my $params = $jboss->get_params_as_hashref(qw/
+        scriptphysicalpath
+        appname
+        keepcontent
+        servergroups
+        allrelevantservergroups
+        additional_options
+    /);
+
+    my $command = qq/undeploy $params->{appname} /;
+
+    if ($params->{keepcontent}) {
+        $command .= ' --keep-content ';
+    }
+    if ($params->{allrelevantservergroups}) {
+        $command .= ' --all-relevant-server-groups ';
+    }
+    elsif ($params->{servergroups}) {
+        $command .= qq/ --server-groups=$params->{servergroups} /;
+    }
+
+    if ($params->{additional_options}) {
+        $params->{additional_options} = $jboss->escape_string($params->{additional_options});
+        $command .= ' ' . $params->{additional_options} . ' ';
+    }
+    my %result = $jboss->run_command($command);
+
+    $jboss->{success_message} = sprintf 'Application %s has been successfully undeployed.', $params->{appname};
+    if ($result{stdout}) {
+        $jboss->out("Command output: $result{stdout}");
+    }
+
+    $jboss->process_response(%result);
 }
 ## === step ends ===
 # Please do not remove the marker above, it is used to place new procedures into this file.
